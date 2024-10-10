e59b81b608a3ecd78298f85411608caaf2fabaf5
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2024-10-10 14:42:23 -0400

    Add files via upload

---
+++ wheel.js
@@ -0,0 +1,60 @@
+const wheel = document.getElementById("wheel");
+const spinButton = document.getElementById("spin-but");
+const finalValue = document.getElementById("final");
+
+//Object that stores values of min and max angle for a value
+const rotationValues = [
+	{ minDeg: 0, maxDeg: 30, value: 1 },
+	{ minDeg: 31, maxDeg: 90, value: 2 },
+	{ minDeg: 91, maxDeg: 150, value: 3 },
+	{ minDeg: 151, maxDeg: 210, value: 4 },
+	{ minDeg: 211, maxDeg: 270, value: 5 },
+	{ minDeg: 271, maxDeg: 330, value: 6 },
+	{ minDeg: 331, maxDeg: 360, value: 1 },
+];
+
+//Size of each piece
+const data = [16,16,16,16,16,16]; //doesn't have to equal 100
+
+//background color of each piece
+var pieColors = [
+	"#56e119",
+	"#FFC300",
+	"#56e119",
+	"#FFC300", 
+	"#56e119",
+	"#FFC300", 
+];
+
+//create chart
+let myChart - new Chart(wheel, {
+	plugins: [ChartDataLabels],
+	type: "pie",
+	data: {
+		labels: [1,2,3,4,5,6],
+		datasets: [
+			{
+				backgroundColor: pieColors,
+				data: data,
+			},
+		],
+	}
+
+options: {
+	responsive: true,
+	animation: { duration: 0},
+	plugins: {
+		tooltip: false;
+		legend: {
+			display: false,
+		},
+		datalabels: {
+			color: "#ffffff",
+			 formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+			font: { size: 24 },
+		},
+	},
+});
+
+
+
