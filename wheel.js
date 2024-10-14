1d48daf22702e916ad755ef844b73a602bb5c3f2
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2024-10-14 12:17:34 -0400

    Update wheel.js
    created RotationValues for all 12 pieces

--- wheel.js
+++ wheel.js
@@ -3,18 +3,31 @@ const spinButton = document.getElementById("spin-but");
 const finalValue = document.getElementById("final");
 
 //Object that stores values of min and max angle for a value
-const rotationValues = [
-	{ minDeg: 0, maxDeg: 30, value: 2 },
-	{ minDeg: 31, maxDeg: 90, value: 1 },
-	{ minDeg: 91, maxDeg: 150, value: 6 },
-	{ minDeg: 151, maxDeg: 210, value: 5 },
-	{ minDeg: 211, maxDeg: 270, value: 4 },
-	{ minDeg: 271, maxDeg: 330, value: 3 },
-	{ minDeg: 331, maxDeg: 360, value: 2 },
+/*
+	Object that stores values of min and max angle for a value 
+	12 pieces 
+*/
+
+const rotationVal = [
+	{ minDeg: 0, maxDeg: 30, value: 3 },
+	{ minDeg: 31, maxDeg: 60, value: 2 },
+	{ minDeg: 61, maxDeg: 90, value: 1 },
+	{ minDeg: 91, maxDeg: 120, value: 12 },
+	{ minDeg: 121, maxDeg: 150, value: 11 },
+	{ minDeg: 151, maxDeg: 180, value: 10 },
+	{ minDeg: 181, maxDeg: 210, value: 9 },
+	{ minDeg: 211, maxDeg: 240, value: 8 },
+	{ minDeg: 241, maxDeg: 270, value: 7 },
+	{ minDeg: 271, maxDeg: 300, value: 6 },
+	{ minDeg: 301, maxDeg: 330 value: 5 },
+	{ minDeg: 331, maxDeg: 360, value: 4 },
+	{ minDeg: 331, maxDeg: 360, value: 3 },
+
+
 ];
 
 //Size of each piece
-const data = [16,16,16,16,16,16]; //doesn't have to equal 100
+const data = [8,8,8,8,8,8,8,8,8,8,8,8]; //doesn't have to equal 100
 
 //background color of each piece
 var pieColors = [
@@ -23,7 +36,13 @@ var pieColors = [
 	"#56e119",
 	"#FFC300", 
 	"#56e119",
+	"#FFC300",
+	"#56e119",
+	"#FFC300",
+	"#56e119",
 	"#FFC300", 
+	"#56e119",
+	"#FFC300", 	
 ];
 
 //create chart
@@ -31,7 +50,7 @@ let myChart - new Chart(wheel, {
 	plugins: [ChartDataLabels],
 	type: "pie",
 	data: {
-		labels: [1,2,3,4,5,6],
+		labels: [1,2,3,4,5,6,7,8,9,10,11,12],
 		datasets: [
 			{
 				backgroundColor: pieColors,
